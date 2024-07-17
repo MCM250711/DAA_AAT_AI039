2c45d465c0d97a3173e395ef167cb2c5acf9c381
Chirag Goyal <106581363+MCM250711@users.noreply.github.com>
2024-07-17 20:46:38 +0530

    CloudyDay.py

--- CloudyDay.py
+++ CloudyDay.py
@@ -1,31 +1,14 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 import operator
-
-
-#
-# Complete the 'maximumPeople' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts following parameters:
-#  1. LONG_INTEGER_ARRAY p
-#  2. LONG_INTEGER_ARRAY x
-#  3. LONG_INTEGER_ARRAY y
-#  4. LONG_INTEGER_ARRAY r
-#
-
 def maximumPeople(people, tower_pos, lclouds, lranges):
     clouds = []
     for c, r in zip(lclouds, lranges):
         clouds.append((max(c - r, 0), c + r))
-    # sort by start
     clouds.sort(key=lambda x: x[0])
-
     towers = []
     for pos, p in zip(tower_pos, people):
         towers.append((pos, p))
@@ -45,7 +28,6 @@ def maximumPeople(people, tower_pos, lclouds, lranges):
     def count(pos, exc=False):
         res = 0
         nonlocal t_idx
-        # uses less than or less or equal operator
         op = operator.lt if exc else operator.le
         while (t_idx < len(towers) and op(towers[t_idx][0], pos)):
             res += towers[t_idx][1]
@@ -70,27 +52,16 @@ def maximumPeople(people, tower_pos, lclouds, lranges):
             covered = 0
             _ = count(cend)
             cend = end
-
     return max_covered + uncovered
-
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input().strip())
-
     p = list(map(int, input().rstrip().split()))
-
     x = list(map(int, input().rstrip().split()))
-
     m = int(input().strip())
-
     y = list(map(int, input().rstrip().split()))
-
     r = list(map(int, input().rstrip().split()))
-
     result = maximumPeople(p, x, y, r)
-
     fptr.write(str(result) + '\n')
-
     fptr.close()
